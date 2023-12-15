ce0a12a1502dfa8b53ddb6fe52e63c4c6834e5cb
Robert Lutz <robertlutzi@gmail.com>
2023-12-15 06:23:43 -0500

    add helper function to gretly reduce algorithmic complexity

--- primenumbergenerator.rkt
+++ primenumbergenerator.rkt
@@ -1,10 +1,6 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname primenumbergenerator) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
-(require 2htdp/image)
-(require 2htdp/universe)
-
-
 ; data definitions
 
 ; A Natural is a non-negative integer
@@ -41,32 +37,29 @@
     [(empty? list) ...]
     [(= (first list) 0) ...]
     [else (... (first list) ... (fn-on-list-of-naturals (rest list)))]))
-    
-
-; constants
-
-(define KNOWNPRIMES '())
 
 
 
 ; functions
 
-(define (prime-number-generator n primes)
-  ; Natural ListOfNaturals -> ListOfNaturals
-  ; given a number and a list of known primes,
-  ;     find all primes less than or equal to that number
+(define (prime-number-generator n)
+  ; Natural -> ListOfNaturals
+  ; given a number n, finds all primes less than or equal to that number
   (cond
-    ;[(not (natural? n)) (error "not a non-negative integer")]
-    ;[(not (list-of-naturals? primes)) (error "not a list of naturals")]
-    [(= n 2) (cons 2 primes)]   ; base case
-    [(is-prime? n (prime-number-generator (- n 1) primes))
-     (cons n (prime-number-generator (- n 1) primes))]  ; dumb
-    [else (prime-number-generator (- n 1) primes)])) ; really dumb
+    [(= n 2) (cons 2 '())]   ; base case
+    [else (prime-sequence-constructor n (prime-number-generator (- n 1)))]))
 ; checks
-(check-expect (prime-number-generator 2 '()) (cons 2 '()))
-(check-expect (prime-number-generator 3 '()) (cons 3 (cons 2 '())))
-(check-expect (prime-number-generator 4 '()) (cons 3 (cons 2 '())))
-(check-expect (prime-number-generator 6 '()) (cons 5 (cons 3 (cons 2 '()))))
+(check-expect (prime-number-generator 2) (cons 2 '()))
+(check-expect (prime-number-generator 3) (cons 3 (cons 2 '())))
+(check-expect (prime-number-generator 4) (cons 3 (cons 2 '())))
+(check-expect (prime-number-generator 6) (cons 5 (cons 3 (cons 2 '()))))
+
+
+(define (prime-sequence-constructor n primes)
+  ; Natural ListOfNaturals -> ListOfNaturals
+  ; if the given n is prime, it prepends n to the list of known primes,
+  ;    otherwise the list of known primes is unchanged
+  (if (is-prime? n primes) (cons n primes) primes))
 
 
 (define (is-prime? n primes)
@@ -85,4 +78,4 @@
 
 ; actions
 
-(prime-number-generator 25 KNOWNPRIMES)
+(prime-number-generator 1000)
